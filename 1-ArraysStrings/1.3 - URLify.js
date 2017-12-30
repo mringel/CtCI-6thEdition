d01cd9382155312fa1dfcdb3588c1d6a9a9b348b
Matthew Ringel <mattew.r.ringel@gmail.com>
2017-12-29 16:59:40 -0800

    added to solution for 1.3 to better deal with trailing spaces

--- 1-ArraysStrings/1.3 - URLify.js
+++ 1-ArraysStrings/1.3 - URLify.js
@@ -22,3 +22,34 @@ function URLify(inputString, trueLength) {
 console.log(URLify('https://www.testsite.com/some text', 34));
 console.log(URLify('https://www.testsite.com/some other text', 40));
 console.log(URLify('https://www.testsite.com/trailing spaces   ', 40));
+
+
+// What if you're not given the "true length" but still need to deal with trailing space?
+
+function URLify2(inputString) {
+  var inputArray = inputString.split('');
+
+  //check for trailing spaces
+  var pointer = inputArray.length-1;
+  var trailingSpaces = (inputArray[pointer] === ' ');
+
+  // start at the end and remove spaces until there are no more spaces
+  while (trailingSpaces) {
+    inputArray.pop();
+    pointer--;
+    trailingSpaces = (inputArray[pointer] === ' ');
+  }
+
+  for (var i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] === ' ') {
+      inputArray[i] = '%20';
+    }
+  }
+
+  return inputArray.join('');
+}
+
+// Tests
+console.log(URLify2('https://www.testsite.com/some text'));
+console.log(URLify2('https://www.testsite.com/some other text'));
+console.log(URLify2('https://www.testsite.com/trailing spaces   '));
